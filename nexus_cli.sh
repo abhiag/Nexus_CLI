dfee90e48f88dbb7ae9e3e9e0f9042e881205854
abhiag <36300569+abhiag@users.noreply.github.com>
2024-12-11 18:26:54 +0530

    Update nexus_cli.sh

--- nexus_cli.sh
+++ nexus_cli.sh
@@ -15,6 +15,25 @@ sudo apt install -y protobuf-compiler
 echo "Installing screen..."
 sudo apt install -y screen
 
+echo "Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Sourcing Rust environment..."
+. "$HOME/.cargo/env"
+
+echo "Adding RISC-V target..."
+rustup target add riscv32i-unknown-none-elf
+
+echo "Installing Nexus cargo tools..."
+cargo install --git https://github.com/nexus-xyz/nexus-zkvm cargo-nexus --tag 'v0.2.4'
+
 # Start a new screen session named "nexusgac"
 echo "Starting screen session nexusgac..."
 screen -S nexusgac
+
+echo "Creating a new Nexus project..."
+cargo nexus new nexus-project
+
+echo "Prover is Starting"
+curl https://cli.nexus.xyz/ | sh
+
