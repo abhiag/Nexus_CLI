238c5ec3d41b7e206fcb34edf7d72d3b583d88f5
abhiag <36300569+abhiag@users.noreply.github.com>
2024-12-11 15:24:20 +0530

    Update cli2.sh

--- cli2.sh
+++ cli2.sh
@@ -4,22 +4,18 @@ echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo "Sourcing Rust environment..."
-source "$HOME/.cargo/env"
+. "$HOME/.cargo/env"
 
 echo "Adding RISC-V target..."
 rustup target add riscv32i-unknown-none-elf
 
 echo "Installing Nexus cargo tools..."
-sudo cargo install --git https://github.com/nexus-xyz/nexus-zkvm cargo-nexus --tag 'v0.2.4'
+cargo install --git https://github.com/nexus-xyz/nexus-zkvm cargo-nexus --tag 'v0.2.4'
 
 echo "Creating a new Nexus project..."
 cargo nexus new nexus-project
 
-if [ -d "nexus-project" ]; then
-  echo "Navigating into the Nexus project directory..."
-  cd nexus-project
-else
-  echo "Failed to create Nexus project directory!"
-fi
+echo "Navigating into the Nexus project directory..."
+cd nexus-project
 
 echo "Nexus project setup is complete!"
