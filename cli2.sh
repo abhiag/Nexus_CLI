e5783c69697de45acdc08e203b98cd5d8f5a7b2d
abhiag <36300569+abhiag@users.noreply.github.com>
2024-12-11 19:40:42 +0530

    Update cli2.sh

--- cli2.sh
+++ cli2.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Display a welcome message
+echo "**********************************************"
+echo "* Final Execution *"
+echo "**********************************************"
+echo "Welcome to the Nexus Prover Node Setup Script!"
+echo "Follow us for updates and support:"
+echo "Telegram: https://t.me/GaCryptOfficial"
+echo "Twitter: https://x.com/GACryptoO"
+echo "**********************************************"
+
 echo "Creating a new Nexus project..."
 cargo nexus new nexus-project
 
